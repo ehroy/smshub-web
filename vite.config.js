6feeb3a4eebce9518f74ecccd84095f850050976
ehroy <68073896+ehroy@users.noreply.github.com>
2024-08-08 01:07:28 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_BASE_URL,
+        target: 'https://smshub.org',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
